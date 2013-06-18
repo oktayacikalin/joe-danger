4c877e8a446af98ddd94831112509eebc1a02667
Oktay Acikalin <oktay.acikalin@gmail.com>
2013-06-18 06:30:34 +0200

    Added abstract obstacle class.

---
+++ data/obstacle.py
@@ -0,0 +1,12 @@
+# Global obstacle functions.
+#
+# @author    Oktay Acikalin <oktay.acikalin@gmail.com>
+# @copyright Oktay Acikalin
+# @license   MIT (LICENSE.txt)
+
+from diamond.sprite import Sprite
+
+
+class AbstractObstacle(Sprite):
+
+    pass
